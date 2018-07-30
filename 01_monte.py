4db0826f94af289fa3a607cb111b8731cb5ab1c1
student <student@Terumasa-no-iMac.local>
2018-07-30 16:07:33 +0900

    modified 01_monte.py

--- 01_monte.py
+++ 01_monte.py
@@ -1,40 +1,48 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Fri Jul 27 19:15:22 2018
+2018/7/27
 
 01_monte.py
 http://aidiary.hatenablog.com/entry/20140620/1403272044
 上のURLを参考にモンテカルロ法に関するプログラムを作る。
 
-一様乱数の生成のプログラム
+一様乱数(Uniform distribution)の生成のプログラム
 """
 
 #一様乱数の生成してヒストグラムを描画
 import numpy as np
 import matplotlib.pyplot as plt
-
-plt.rcParams['figure.figsize'] = (10, 8)
-np.random.seed()
-N = 10000
-x = np.random.uniform(0.0, 1.0, N)
-nbins = 100
-plt.hist(x, nbins, normed=True)
-plt.show()
-
 from scipy.stats import uniform
 
-#一様分布に従う確率分布からランダムサンプリング
-np.random.seed()
-#[0.0, 1.0]の一様分布に従う確率変数
-rv = uniform(loc=0.0, scale=1.0)
-#一様分布からサンプリング
-x = rv.rvs(size=N)
-plt.hist(x, nbins, normed=True)
+print("numpy or scipy?")
+selct = input()
 
-#真のPDFを描画
-x = np.linspace(rv.pdf(0), rv.pdf(1), 100)
-plt.plot(x, uniform.pdf(x), 'r-', lw=2, label='uniform pdf')
+#numpyを使って一様乱数の生成
+if selct == "numpy":
+    #plt.rcParams['figure.figsize'] = (10, 10)
+    np.random.seed()
+    N = 10000
+    x = np.random.uniform(0.0, 1.0, N)
+    nbins = 50
+    plt.hist(x, nbins, normed=True, label="frozen pdf")
+    plt.show()
 
-plt.show()
+#scipy.stats用いて一様乱数の生成
+if selct == "scipy":
+    #一様分布に従う確率分布からランダムサンプリング
+    np.random.seed()
+    N = 10000
+    #[0.0, 1.0]の一様分布に従う確率変数
+    rv = uniform(loc=0.0, scale=1.0)
+    #一様分布からサンプリング
+    x = rv.rvs(size=N)
+    nbins = 50
+    plt.hist(x, nbins, normed=True, label='frozen pdf')
+    
+    #真のPDFを描画
+    x = np.linspace(uniform.ppf(0.01), uniform.ppf(0.99), 100)    
+    plt.plot(x, uniform.pdf(x), 'r-', lw=4, label='uniform pdf')    #lwは線の太さ
+    plt.legend()
+    plt.show()
 
--- README.txt
+++ README.txt
@@ -6,3 +6,8 @@ http://aidiary.hatenablog.com/entry/20140620/1403272044
 01_monte.py	一様乱数の生成
 02_monte.py	逆変換法
 03_monte.py	逆変換法(2)
+04_monte.py	受理・棄却法
+
+
+
+
